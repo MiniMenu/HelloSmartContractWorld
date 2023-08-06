4b96b3430655d3638108b5bb1350a3a5b299f8df
MiniMenu <darkhorseminal@gmail.com>
2023-08-06 09:33:08 +0530

    My First Remix Commit

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract HelloWorld {
+    function sayHelloToTheContract() public pure returns (string memory){
+        return "Hello Smart Contract World";
+    }
+}
